11843b1493961c9655d95a1ad47fae370915fcde
millan-figueroa <millan.fig@gmail.com>
2025-11-27 20:36:55 -0800

    Adds placeholder text for home and contact pages, adds route handlers for homepage and contact pages.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // 1. Import the express library
 const express = require("express");
+const path = require("path");
 
 // 2. Create an instance of an Express application
 const app = express();
@@ -7,9 +8,14 @@ const app = express();
 // 3. Define the port the server will run on
 const port = 3000;
 
-// 4. Define a route handler for GET requests to the root URL ('/')
+// Route for the homepage, sends index.html
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Route for /contact sends contact.html
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "contact.html"));
 });
 
 // 5. Start the server and have it listen for incoming connections
